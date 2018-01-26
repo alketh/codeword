5fd0e13b4719894ffbd00fb3a5ddf484874722d6
Alexander Keth <alexander.keth@posteo.net>
2018-01-26 01:16:27 +0100

    Add dummy scipt to work with.

---
+++ clean-script.R
@@ -0,0 +1,6 @@
+load("data/dummy_script.rda", verbose = T)
+script <- dummy_script
+
+clean_script <- function(script) {
+  
+}
---
+++ data-raw/fluff.R
@@ -0,0 +1,5 @@
+xxx <- readLines("data-raw/agg-data.R")
+
+dummy_script <- xxx
+
+save(dummy_script, file = "data/dummy_script.rda")
