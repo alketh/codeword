ddf0ac292bd8140e7329e6a8034a19ca56f17737
Alexander Keth <alexander.keth@posteo.net>
2018-01-26 01:28:47 +0100

    Get code words from random R-script.

--- clean-script.R
+++ clean-script.R
@@ -1,6 +1,18 @@
-load("data/dummy_script.rda", verbose = T)
-script <- dummy_script
+# load("data/dummy_script.rda", verbose = T)
+# script <- dummy_script
 
 clean_script <- function(script) {
+  # Remove lines with comments
+  pos <- stringr::str_locate(script, pattern = "#")[, 1]
+  pos <- which(pos == 1)
   
+  cs <- script
+  if (length(pos) >= 1) cs <- script[-pos]
+  
+  # Extract code language using the tokenizer Package
+  out <- tokenizers::tokenize_words(cs)
+  
+  # Do some cleaning
+  out <- out[vapply(out, length, FUN.VALUE = integer(1)) != 0]
+  return(unlist(out))
 }
